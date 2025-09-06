9b399458d64b16dfc79a35e055688a895c0b9071
nicolehoppy <nikoletta.chmielewska@gmail.com>
2025-09-06 15:35:37 +0200

    Initial commit

---
+++ src/test/java/pl/nicolehoppy/eventmanager/EventmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nicolehoppy.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
